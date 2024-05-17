b78cc8695ed909198532e1c7b4d037d06dd3b566
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-05-17 16:33:53 +1000

    fix: update entry functionality

--- src/components/Entry.jsx
+++ src/components/Entry.jsx
@@ -1,13 +1,12 @@
+
 export default function Entry(props){
-    return (
-        <div>
-            <h1>{props.entryData.title}</h1>
-            <h4>{props.entryData.author}</h4>
-            <h6>{props.entryData.date}</h6>
+	return (
+		<div>
+			<h1>{props.entryData.title}</h1>
+			<h4>{props.entryData.author}</h4>
+			<h6>{props.entryData.date}</h6>
 
-            <p>{props.entryData.content}</p>
-
-        </div>
-
-    )
+			<p>{props.entryData.content}</p>
+		</div>
+	)
 }
--- src/components/EntryParent.jsx
+++ src/components/EntryParent.jsx
@@ -2,16 +2,15 @@ import { useState } from "react";
 import Entry from "./Entry";
 import EntryForm from "./EntryForm";
 
-
 export default function EntryParent(props){
 
-    let [editMode, setEditMode] = useState(false);
+	let [editMode, setEditMode] = useState(false);
 
-    return(
-        <section>
-            {editMode ? < EntryForm entryData={props.entryData} addEntry={props.addEntry}/> : <Entry entryData={props.entryData}/>}
+	return(
+		<section>
+			{editMode ? <EntryForm entryData={props.entryData} addEntry={props.addEntry} /> : <Entry entryData={props.entryData} /> }
 
-            <button onClick={() => setEditMode(!editMode)}> Toggel Edit Mode</button>
-        </section>
-    )
+			<button onClick={() => setEditMode(!editMode)}>Toggle Edit Mode</button>
+		</section>
+	)
 }
