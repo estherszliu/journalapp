eba4ad319cac88f0f8eb50786e70af0711a99d47
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-05-17 16:06:01 +1000

    feat: journal entries rendering and creating

--- src/components/EntryForm.jsx
+++ src/components/EntryForm.jsx
@@ -10,20 +10,26 @@ import { useState } from "react"
 
 export default function EntryForm(props){
 
-    let [localTitle, setLocalTitle] = useState(props.entryData.title);
-    let [locatAuthor, setLocalAuthor] = useState(props.entryData.author);
-    let [locatContent, setLocalContent] = useState(props.entryData.content);
+    let [localTitle, setLocalTitle] = useState((props.entryData && props.entryData.title) || "Default Title");
+    let [localAuthor, setLocalAuthor] = useState(props.entryData ? props.entryData.author : "Default Author");
+    let [localContent, setLocalContent] = useState(props.entryData?.content || "Hello world! Write a journal entry!");
+
+
     return (
         <div>
             <lable htmlFor="entryTitle">Title:</lable>
-            <inpupt type="text" name="entryTitle" class="entryTitle" value={localTitle} onChange={(event) => setLocalTitle(envent.target.value)} />
+            <inpupt type="text" name="entryTitle" className="entryTitle" value={localTitle} onChange={(event) => setLocalTitle(event.target.value)} />
 
             <lable htmlFor="entryAuthor">Author:</lable>
-            <inpupt type="text" name="entryAuthor" class="entryAuthor" value={localTitle} onChange={(event) => setLocalAuthor(envent.target.value)} />
+            <inpupt type="text" name="entryAuthor" className="entryAuthor" value={localTitle} onChange={(event) => setLocalAuthor(event.target.value)} />
 
 
             <lable htmlFor="entryContent">Content:</lable>
-            <inpupt type="text" name="entryContent" class="entryContent" value={localTitle} onChange={(event) => setLocalContent(envent.target.value)} />
+            <inpupt type="text" name="entryContent" className="entryContent" value={localTitle} onChange={(event) => setLocalContent(event.target.value)} />
+
+            <button onClick={() => props.addEntry(localTitle, localAuthor, localContent)}>
+                Submit entry
+            </button>
         </div>
     )
 }
