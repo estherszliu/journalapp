3e011d1991fbd9c5ae1591346c8e264890fc3c2e
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-05-19 16:08:10 +1000

    feat: add & edit blog post working via context

--- src/contexts/BlogContext.jsx
+++ src/contexts/BlogContext.jsx
@@ -3,15 +3,15 @@ import { useContext, createContext, useState, useEffect } from "react";
 import { useLocalStorage } from "react-use";
 
 
-let defaultJournalData = [
-    {
-        id: 0,
-        title: "Default journal post",
-        content: "Welcome to the website!",
-        author: "Alex",
-        data: Date.now()
-    }
-];
+// let defaultJournalData = [
+//     {
+//         id: 0,
+//         title: "Default journal post",
+//         content: "Welcome to the website!",
+//         author: "Alex",
+//         data: Date.now()
+//     }
+// ];
 
 // this is only the data
 export const JournalDataContext = createContext(null);
@@ -35,7 +35,7 @@ export function BlogProvider(children){
     let [journalEntries, setJournalEntries] = useState([]);
     let [storedEntries, setStoredEntries] = useLocalStorage("journalEntries", []);
 
-    let [exampleState, setExampleState] = useState("Hello from global level!");
+    // let [exampleState, setExampleState] = useState("Hello from global level!");
 
 
     useEffect(() => {
@@ -47,9 +47,40 @@ export function BlogProvider(children){
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
+    const addEntryToJournal = (title, author, content, date = Date.now(), id = null) => {
+        // let currentJournalEntries = [...journalEntries];
+        // currentJournalEntries.push({title, author, content, date});
+        // setJournalEntries(currentJournalEntries);
+    
+        // crypto.uuid as the key
+        // use the date/datetime/milliseconds as the key
+        // combine all data into a string title+content+date 
+        // simple number as ID eg. 0, 1, 2, 
+    
+        if (id){
+          // UPDATE EXISTING ENTRY 
+          // find the object with the matching ID 
+          let existingEntry = journalEntries.find((entry) => entry.id === id);
+          // remove the found object from the array
+          let currentJournalEntries = journalEntries.filter((entry) => entry.id !== id);
+          // update the found object 
+          existingEntry = {
+            title, author, content, date, id
+          }
+          // add the found object back into the array
+          let updatedJournalEntries = [...currentJournalEntries, existingEntry];
+          // write array to state & localstorage 
+          setJournalEntries(updatedJournalEntries);
+          setStoredEntries(updatedJournalEntries);
+        } else {
+          // CREATE A NEW ENTRY 
+          setJournalEntries([...journalEntries, { id: journalEntries.length, title, author, content, date }]);
+          setStoredEntries([...journalEntries, { id: journalEntries.length,title, author, content, date }]);
+        }
+    }
     return (
         <JournalDataContext.Provider value={journalEntries}>
-            <JournalDispatchContext.Provider value={setJournalEntries}>
+            <JournalDispatchContext.Provider value={addEntryToJournal}>
                 {children}
             </JournalDispatchContext.Provider>
         </JournalDataContext.Provider>
