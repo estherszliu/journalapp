9c4de66e8940f1023d097fae93d0a30c39b058ff
Esther Dennis <148415275+estherszliu@users.noreply.github.com>
2024-05-19 17:07:53 +1000

    feat: homepage rendering content

--- src/components/EntryParent.jsx
+++ src/components/EntryParent.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Entry from "./Entry";
 import EntryForm from "./EntryForm";
+import { useJournalData } from "../contexts/BlogContext";
 
 export default function EntryParent(props){
 
 	let [editMode, setEditMode] = useState(false);
 
+    let [journalEntry, setJournalEntry] = useState({});
+
+    let journalEntries = useJournalData();
+
+    useEffect(() => {
+        let foundEntry = journalEntries.find(entry => entry.id === props.id);
+
+        setJournalEntry(foundEntry)
+
+    },[]);
+
+
 	return(
 		<section>
-			{editMode ? <EntryForm entryData={props.entryData} /> : <Entry entryData={props.entryData} /> }
+			{editMode ? <EntryForm entryData={journalEntry} /> : <Entry entryData={props.entryData} /> }
 
 			<button onClick={() => setEditMode(!editMode)}>Toggle Edit Mode</button>
 		</section>
